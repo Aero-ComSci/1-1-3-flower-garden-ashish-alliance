6e4c73b75cbf7e79849cb03551cb287f0f8467de
Akshat Verma <157422041+the-ak95@users.noreply.github.com>
2024-08-30 14:52:25 -0700

    Create stem.py
    stem for travyn

---
+++ stem.py
@@ -0,0 +1,14 @@
+# stem
+painter.color("green")
+painter.pensize(15)
+painter.goto(0, -150)
+painter.setheading(90)
+painter.forward(100)
+#  leaf
+painter.setheading(270)
+painter.circle(20, 120, 20)
+painter.setheading(90)
+painter.goto(0, -60)
+# rest of stem
+painter.forward(60)
+painter.setheading(0)
